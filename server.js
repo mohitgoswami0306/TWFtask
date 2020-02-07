e9244d4f659d1f89c4a6921ac40b2de6e3f30f9f
mohitgoswami0306 <mohitgoswami0306@gmail.com>
2020-02-07 18:10:32 +0530

    Second commit

--- server.js
+++ server.js
@@ -7,7 +7,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use("/", Output);
 
-const server = app.listen(8080, function() {
+const PORT = process.env.PORT || 8080;
+
+const server = app.listen(PORT, function() {
   console.log("Server is listening on port 8080");
 });
 
